51fea7299d9d0c040d37a963ad207e21f7538ba5
jaclynrbeck <jaclyn.r.beck@gmail.com>
2026-06-15 12:56:56 -0700

    Added boot.py to only mount as drive if key 1 held down

---
+++ boot.py
@@ -0,0 +1,14 @@
+import board
+import digitalio
+import storage
+
+# Mount the CIRCUITPY drive as read-only unless the top-left key is held down
+encoder_switch = digitalio.DigitalInOut(board.KEY1)
+encoder_switch.switch_to_input(pull=digitalio.Pull.UP)
+if(encoder_switch.value):
+    print("Disabling USB Storage")
+    storage.disable_usb_drive()
+else:
+    print("Mounting Read/Write")
+    storage.enable_usb_drive()
+
